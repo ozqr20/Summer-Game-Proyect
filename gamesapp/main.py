f0ec8b28fcc701afde7fbde64a80db3c9d784385
USER <b_sanchez5@yahoo.com>
2019-08-20 12:13:18 -0700

    deleted import

--- gamesapp/main.py
+++ gamesapp/main.py
@@ -1,7 +1,6 @@
 import webapp2
 import jinja2
 import os
-import game
 the_jinja_env = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
     extensions=['jinja2.ext.autoescape'],
