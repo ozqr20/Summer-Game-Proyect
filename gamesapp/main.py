a27d71db1a4eb5f67438d5cc5c53a8f67af23b08
USER <oscar_manosalva@elcamino.edu>
2019-08-21 08:56:46 -0700

    class removed

--- gamesapp/main.py
+++ gamesapp/main.py
@@ -20,16 +20,8 @@ class EnterInfoHandler2(webapp2.RequestHandler):
     def post(self):
         template = the_jinja_env.get_template("tetris.html")
         self.response.write(template.render())
-class EnterInfoHandler3(webapp2.RequestHandler):
-    def get(self):
-        template = the_jinja_env.get_template("battleship.html")
-        self.response.write(template.render())
-    def post(self):
-        template = the_jinja_env.get_template("battleship.html")
-        self.response.write(template.render())
 
 app = webapp2.WSGIApplication([
     ('/', EnterInfoHandler),
     ('/tetris.html', EnterInfoHandler2),
-    ('/battleship.html', EnterInfoHandler3),
 ], debug=True)
